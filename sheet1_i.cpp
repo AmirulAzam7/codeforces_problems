cbb546bda1e08a9f68e8e78529ec650369e2d08a
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-08-01 16:46:05 +0600

    Create sheet1_i.cpp

---
+++ sheet1_i.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  long long int a,b;
+
+  cin>>a>>b;
+
+  if(a>b || a==b)
+  {
+      cout<<"Yes";
+  }
+  else
+  {
+      cout<<"No";
+  }
+
+
+
+    return 0;
+}
+
