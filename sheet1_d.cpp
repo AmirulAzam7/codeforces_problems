e1ab99cbcfb62c271b4f188f07e2ead32bdc475e
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-08-01 16:22:32 +0600

    Create sheet1_d.cpp

---
+++ sheet1_d.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    long long int a,b,c,d,e;
+    cin>>a>>b>>c>>d;
+
+    e = (a*b)-(c*d);
+
+    cout<<"Difference = "<<e;
+
+
+    return 0;
+}
