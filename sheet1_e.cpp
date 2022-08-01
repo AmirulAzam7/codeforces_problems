14189fa0210b9b16bf97aa7eb6fd619b35b09910
Amirul Azam <109798823+AmirulAzam7@users.noreply.github.com>
2022-08-01 16:24:39 +0600

    Create sheet1_e.cpp

---
+++ sheet1_e.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    double r,area;
+    cin>>r;
+    cout<<fixed<<setprecision(9);
+
+    area = 3.141592653 * (r*r);
+
+    cout<<area;
+
+
+
+    return 0;
+}
